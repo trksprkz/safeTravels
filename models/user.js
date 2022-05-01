3dffa105f56f07b8f49c6fd8058ef69aed4e2eac
Issac Davis <54684726+trksprkz@users.noreply.github.com>
2022-05-01 05:54:07 -0700

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,17 +1,15 @@
-var mongoose = require("mongoose");
-var passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-var UserSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  email: String,
-  phone: String,
-  fullName: String,
-  image: String,
-  imageId: String,
-  joined: { type: Date, default: Date.now }
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
